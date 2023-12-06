8661a40a4a094d651f53bda6302236fdeb5664b3
Abhisek Dubey <abhidb95@gmail.com>
2023-12-06 22:54:57 +0530

    Lec 3: Http Methods

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ const url = require('url');
 const myServer = http.createServer((req, res) => {
     if (req.url === '/favicon.ico') return res.end()
     // create log file
-    const log = `${Date.now()}: ${req.url} New Req Received\n`
+    const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`
     // url parse
     const myUrl = url.parse(req.url, true)
     console.log(myUrl);
     fs.appendFile('log.txt', log, (err, data) => {
         switch (myUrl.pathname) {
-            case '/': res.end('Home Page')
+            case '/':
+                if (req.method === "GET") res.end('Home Page')
                 break;
             case '/about':
                 const username = myUrl.query.myname;
                 res.end(`Hi ${username}`)
                 break
+            case '/signup':
+                if (req.method === 'GET') res.end('This is a sign up form')
+                else if (req.method === 'POST') {
+                    // DB Query
+                    res.end('Success')
+                }
             default: res.end('404 Page Not Found')
         }
     })
