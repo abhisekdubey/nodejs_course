5626ddc6fa23fa138dc99d036f72e9c785480e14
Abhisek Dubey <abhidb95@gmail.com>
2023-12-06 22:29:19 +0530

    Lec 2: Handling URLs

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const http = require('http');
 const fs = require('fs')
+const url = require('url');
 
 const myServer = http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') return res.end()
     // create log file
     const log = `${Date.now()}: ${req.url} New Req Received\n`
+    // url parse
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl);
     fs.appendFile('log.txt', log, (err, data) => {
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case '/': res.end('Home Page')
                 break;
-            case '/about': res.end('I am Abhisek Dubey')
+            case '/about':
+                const username = myUrl.query.myname;
+                res.end(`Hi ${username}`)
                 break
             default: res.end('404 Page Not Found')
         }
